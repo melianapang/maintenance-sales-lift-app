e7455d83cbf91c9f15e8322fdec8e24cb46a8f31
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-24 17:51:52 +0700

    chore(padding-utils): update to get padding

--- lib/core/utilities/padding_utils.dart
+++ lib/core/utilities/padding_utils.dart
@@ -8,4 +8,21 @@ class PaddingUtils {
     final double bottomPadding = MediaQuery.of(context).viewPadding.bottom;
     return bottomPadding > 0 ? bottomPadding : defaultPadding;
   }
+
+  static EdgeInsets getPadding(
+    BuildContext context, {
+    double defaultPadding = 8,
+  }) {
+    final double topPadding = MediaQuery.of(context).viewPadding.top;
+    final double bottomPadding = MediaQuery.of(context).viewPadding.bottom;
+    final double leftPadding = MediaQuery.of(context).viewPadding.left;
+    final double rightPadding = MediaQuery.of(context).viewPadding.right;
+
+    return EdgeInsets.only(
+      top: topPadding > 0 ? topPadding : defaultPadding,
+      bottom: bottomPadding > 0 ? bottomPadding : defaultPadding,
+      left: leftPadding > 0 ? leftPadding : defaultPadding,
+      right: rightPadding > 0 ? rightPadding : defaultPadding,
+    );
+  }
 }
