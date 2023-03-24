747dead71ab7f744d043a44c8bb007e0ec478a23
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-24 17:52:21 +0700

    chore(role-model): add new SuperAdmin role

--- lib/core/models/role_model.dart
+++ lib/core/models/role_model.dart
@@ -1,1 +1,14 @@
-enum Role { Admin, Sales, Engineers }
+enum Role { SuperAdmin, Admin, Sales, Engineers }
+
+String mappingRole(Role role) {
+  switch (role) {
+    case Role.SuperAdmin:
+      return "Super Admin";
+    case Role.Admin:
+      return "Admin";
+    case Role.Sales:
+      return "Sales";
+    case Role.Engineers:
+      return "Teknisi";
+  }
+}
