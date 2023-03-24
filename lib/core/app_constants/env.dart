df15ac04045fec8b424cc9396a342c704d6ce763
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-24 20:33:59 +0700

    chores(env): manage environment

---
+++ lib/core/app_constants/env.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class EnvConstants {
+  static EnvironmentEnum _getEnvEnum(String environment) =>
+      envMapping[environment] ?? EnvironmentEnum.dev;
+
+  static EnvironmentEnum get env {
+    final env = dotenv.env['ENV'];
+    return _getEnvEnum(env ?? '');
+  }
+
+  static String get baseURL {
+    return dotenv.env['BASE_URL'] ?? '';
+  }
+}
+
+enum EnvironmentEnum {
+  dev,
+  staging,
+  production,
+}
+
+extension EnvironmentEnumExtension on EnvironmentEnum {
+  static const Map<EnvironmentEnum, String> values = <EnvironmentEnum, String>{
+    EnvironmentEnum.dev: 'dev',
+    EnvironmentEnum.staging: 'staging',
+    EnvironmentEnum.production: 'production',
+  };
+
+  String get value => values[this] ?? '';
+}
+
+Map<String, EnvironmentEnum> envMapping = <String, EnvironmentEnum>{
+  EnvironmentEnum.dev.value: EnvironmentEnum.dev,
+  EnvironmentEnum.staging.value: EnvironmentEnum.staging,
+  EnvironmentEnum.production.value: EnvironmentEnum.production,
+};
