7aaa87e4325f4ec763d323537b893589d2713144
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-24 20:21:39 +0700

    feat(set-password-for-user): create viewmodel

---
+++ lib/core/viewmodels/user/set_password_user_view_model.dart
@@ -0,0 +1,43 @@
+import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
+
+class SetPasswordViewUserModel extends BaseViewModel {
+  SetPasswordViewUserModel();
+
+  String _newPassword = "";
+  String get newPassword => _newPassword;
+
+  String _newConfirmPassword = "";
+  String get newConfirmPassword => _newConfirmPassword;
+
+  bool? _isValid;
+  bool? get isValid => _isValid;
+
+  bool _showNewPassword = false;
+  bool get showNewPassword => _showNewPassword;
+
+  bool _showConfirmNewPassword = false;
+  bool get showConfirmNewPassword => _showConfirmNewPassword;
+
+  @override
+  Future<void> initModel() async {}
+
+  void setNewPassword(String value) {
+    _newPassword = value;
+    notifyListeners();
+  }
+
+  void setNewConfirmPassword(String value) {
+    _newConfirmPassword = value;
+    notifyListeners();
+  }
+
+  void setShowNewPassword() {
+    _showNewPassword = !_showNewPassword;
+    notifyListeners();
+  }
+
+  void setShowConfirmNewPassword() {
+    _showConfirmNewPassword = !_showConfirmNewPassword;
+    notifyListeners();
+  }
+}
