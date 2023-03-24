f4a8c6eb2a47437c19293ec35c11c7d887fb9a1b
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-25 01:19:31 +0700

    feat(thumbnail-video): generate thumbnail video

---
+++ lib/core/utilities/thumbnail_video_utils.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:rejo_jaya_sakti_apps/core/models/gallery_data_model.dart';
+import 'package:rejo_jaya_sakti_apps/ui/shared/loading.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
+
+class ThumbnailResult {
+  final Image? image;
+  final int? dataSize;
+  final int? height;
+  final int? width;
+  const ThumbnailResult({this.image, this.dataSize, this.height, this.width});
+}
+
+class ThumbnailVideoUtils {
+  static FutureBuilder<ThumbnailResult> showThumbnailVideo(GalleryData data) {
+    return FutureBuilder<ThumbnailResult>(
+      future: generateVideoThumbnail(data.filepath),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          final _image = snapshot.data.image;
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              _image,
+            ],
+          );
+        } else if (snapshot.hasError) {
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            color: Colors.red,
+            child: Text(
+              "Error:\n${snapshot.error.toString()}",
+            ),
+          );
+        } else {
+          return buildLoadingPage();
+        }
+      },
+    );
+  }
+
+  static Future<ThumbnailResult> generateVideoThumbnail(String fileUrl) async {
+    final Completer<ThumbnailResult> completer = Completer();
+
+    String? thumbnailFileName = await VideoThumbnail.thumbnailFile(
+      video: fileUrl,
+      thumbnailPath: (await getTemporaryDirectory()).path,
+      imageFormat: ImageFormat.JPEG,
+      quality: 65,
+    );
+
+    final file = File(thumbnailFileName!);
+    Uint8List bytes = file.readAsBytesSync();
+    int _imageDataSize = bytes.length;
+    print("image size: $_imageDataSize");
+
+    final _image = Image.memory(
+      bytes,
+      fit: BoxFit.cover,
+    );
+    _image.image
+        .resolve(ImageConfiguration())
+        .addListener(ImageStreamListener((ImageInfo info, bool _) {
+      completer.complete(ThumbnailResult(
+        image: _image,
+        dataSize: _imageDataSize,
+        height: info.image.height,
+        width: info.image.width,
+      ));
+    }));
+
+    return completer.future;
+  }
+}
