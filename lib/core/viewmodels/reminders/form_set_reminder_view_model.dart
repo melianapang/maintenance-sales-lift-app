92d5ce7c9e82f049505c8a37730db40fa9c2ad56
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-24 20:26:47 +0700

    chore(set-reminder): create viewmodel

---
+++ lib/core/viewmodels/reminders/form_set_reminder_view_model.dart
@@ -0,0 +1,36 @@
+import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
+import 'package:rejo_jaya_sakti_apps/ui/widgets/filter_menu.dart';
+
+class FormSetReminderViewModel extends BaseViewModel {
+  FormSetReminderViewModel();
+
+  // Filter related
+  int _selectedSetReminderForOption = 0;
+  int get selectedSetReminderForOption => _selectedSetReminderForOption;
+  final List<FilterOption> _setReminderForOption = [
+    FilterOption("Konfirmasi Pertama", true),
+    FilterOption("Konfirmasi Kedua", false),
+    FilterOption("Konfirmasi Ketiga", false),
+    FilterOption("Nego", false),
+    FilterOption("Instalasi", false),
+    FilterOption("Selesai", false),
+    FilterOption("Batal", false),
+  ];
+  List<FilterOption> get setReminderForOption => _setReminderForOption;
+  // End of filter related
+
+  @override
+  Future<void> initModel() async {}
+
+  void setHasilKonfirmasi(int index) {
+    _selectedSetReminderForOption = index;
+    for (int i = 0; i < _setReminderForOption.length; i++) {
+      if (i == _selectedSetReminderForOption) {
+        _setReminderForOption[i].isSelected = true;
+        continue;
+      }
+      _setReminderForOption[i].isSelected = false;
+    }
+    notifyListeners();
+  }
+}
