c7190db81d15d854846ad07b55d7af32cb383a6f
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-24 21:36:12 +0700

    chore(api): refactor name

--- lib/core/apis/api.dart
+++ lib/core/apis/api.dart
@@ -11,11 +11,6 @@ part 'api.g.dart';
 abstract class Api {
   factory Api(Dio dio, {String baseUrl}) = _Api;
 
-  @POST('/account/phone-number/check')
-  Future<HttpResponse<dynamic>> checkPhoneNumber(
-    @Body() CheckNumberRequest request,
-  );
-
   @POST('/project-lift/api/0/Auth/login')
   Future<HttpResponse<dynamic>> requestLogin(
     @Body() LoginRequest request,
@@ -24,33 +19,16 @@ abstract class Api {
 
 class ApiService {
   ApiService({
-    required this.authenticationApi,
+    required this.api,
   });
 
-  final Api authenticationApi;
-
-  Future<bool> checkPhoneNumber({required String phoneNumber}) async {
-    try {
-      final payload = CheckNumberRequest(phoneNumber: phoneNumber);
-      final HttpResponse<dynamic> response =
-          await authenticationApi.checkPhoneNumber(payload);
-
-      if (response.response.statusCode == 200) {
-        return true;
-      }
-      return false;
-    } catch (e) {
-      log("Sequence number error");
-      return false;
-    }
-  }
+  final Api api;
 
   Future<String?> requestLogin(
       {required String inputUser, required String password}) async {
     try {
       final payload = LoginRequest(inputUser: inputUser, password: password);
-      final HttpResponse<dynamic> response =
-          await authenticationApi.requestLogin(payload);
+      final HttpResponse<dynamic> response = await api.requestLogin(payload);
 
       if (response.response.statusCode == 200) {
         LoginResponse loginResponse =
