d67ec5cfe8854746e78b0e6ce24b90eddb335248
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-24 20:25:05 +0700

    chore(view-model): create base viewmodel

--- lib/core/viewmodels/view_model.dart
+++ lib/core/viewmodels/view_model.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
+import 'package:provider/provider.dart';
 
 class ViewModel<T extends BaseViewModel> extends StatefulWidget {
   const ViewModel({
