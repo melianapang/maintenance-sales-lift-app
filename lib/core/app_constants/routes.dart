3d721bb3bfdb850fa82d4c65630b4bb116142430
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-24 17:47:37 +0700

    chore: update routes

--- lib/core/app_constants/routes.dart
+++ lib/core/app_constants/routes.dart
@@ -2,38 +2,43 @@ class Routes {
   static const String splashScreen = 'splash-screen';
   static const String login = 'login';
   static const String home = 'home';
-  static const String map = 'map';
+  static const String map = "map";
 
   static const String editProfile = 'edit-profile';
   static const String editCustomer = 'edit-customer';
   static const String changePassword = 'change-password';
-
   static const String listApproval = 'list-approval';
   static const String listCustomer = 'list-customer';
   static const String listFollowUp = 'list-follow-up';
   static const String listReminder = 'list-reminder';
   static const String listMaintenance = 'list-maintenance';
   static const String listLog = 'list-log';
-  static const String listSales = 'list-sales';
-  static const String listUnits = 'list-units';
-
+  static const String listUser = 'list-user';
+  static const String listProjects = 'list-project';
   static const String detailApproval = 'detail-approval';
+  static const String detailUser = 'detail-user';
+  static const String detailProject = 'detail-project';
   static const String detailCustomer = 'detail-customer';
   static const String detailLog = 'detail-log';
   static const String detailMaintenance = 'detail-pemeliharaan';
   static const String detailHistoryMaintenance = 'detail-history-maintenance';
   static const String detailFollowUp = 'detail-follow-up';
   static const String detailHistoryFollowUp = 'detail-history-followUp';
-  static const String detailSales = 'detail-sales';
-  static const String detailUnit = 'detail-unit';
-
+  static const String detailReminder = 'detail-reminder';
+  static const String addCustomer = 'add-customer';
+  static const String addUser = 'add-user';
+  static const String editUser = 'edit-user';
+  static const String addProject = 'add-project';
+  static const String editProject = 'edit-project';
   static const String formFollowUp = 'form-follow-up';
+  static const String formMaintenance = 'form-maintenance';
   static const String formSetReminder = 'form-set-reminder';
-
-  static const String addSales = 'add-sales';
-  static const String editSales = 'edit-sales';
-  static const String addUnit = 'add-unit';
-  static const String editUnit = 'edit-unit';
-
+  static const String setPasswordUser = 'set-password-user';
+  static const String uploadPO = 'form-upload-po';
+  static const String afterSetReminder = "after-set-reminder";
   static const String exportCustomer = 'export-customer-data';
+  static const String exportMaintenance = 'export-maintenance-data';
+
+  // Gallery
+  static const String imageDetail = 'image-detail';
 }
