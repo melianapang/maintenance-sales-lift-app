ed9e5201664f6a517505d08d5a233226f55c9e68
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-26 22:40:06 +0700

    feat(detail-log): update log view

--- lib/ui/views/log/detail_log_view.dart
+++ lib/ui/views/log/detail_log_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:rejo_jaya_sakti_apps/core/app_constants/colors.dart';
 import 'package:rejo_jaya_sakti_apps/core/utilities/text_styles.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/spacings.dart';
-import 'package:rejo_jaya_sakti_apps/ui/widgets/buttons.dart';
 import 'package:rejo_jaya_sakti_apps/ui/widgets/text_inputs.dart';
 
 import '../../shared/app_bars.dart';
@@ -40,52 +39,138 @@ class _DetailLogViewState extends State<DetailLogView> {
               ),
               Spacings.vert(24),
               TextInput.disabled(
-                label: "Permintaan ubah pada tanggal",
+                label: "Jenis Data yang diubah",
               ),
               Spacings.vert(24),
               TextInput.disabled(
-                label: "Disetujui pada tanggal",
+                label: "ID Data",
               ),
-              Spacings.vert(32),
+              Spacings.vert(12),
               const Divider(
-                thickness: 2,
+                thickness: 0.5,
                 color: MyColors.yellow,
               ),
-              Spacings.vert(10),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Data yang diubah",
-                  style: buildTextStyle(
-                    fontSize: 20,
-                    fontColor: MyColors.yellow01,
-                    fontWeight: 800,
+              Spacings.vert(12),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Sebelum",
+                      textAlign: TextAlign.center,
+                      style: buildTextStyle(
+                        fontSize: 20,
+                        fontColor: MyColors.yellow01,
+                        fontWeight: 800,
+                      ),
+                    ),
                   ),
+                  Expanded(
+                    child: Text(
+                      "Sesudah",
+                      textAlign: TextAlign.center,
+                      style: buildTextStyle(
+                        fontSize: 20,
+                        fontColor: MyColors.yellow01,
+                        fontWeight: 800,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Spacings.vert(16),
+              ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 10,
+                separatorBuilder: (context, index) => const Divider(
+                  color: MyColors.lightBlack01,
+                  thickness: 0.4,
                 ),
-              ),
-              Spacings.vert(24),
-              TextInput.disabled(
-                label: "Nomor Pelanggan",
-              ),
-              Spacings.vert(24),
-              TextInput.disabled(
-                label: "Nama Pelanggan",
-              ),
-              Spacings.vert(24),
-              TextInput.disabled(
-                label: "Kota",
-              ),
-              Spacings.vert(24),
-              TextInput.disabled(
-                label: "No Telepon",
-              ),
-              Spacings.vert(24),
-              TextInput.disabled(
-                label: "Email",
+                itemBuilder: (BuildContext context, int index) {
+                  return _buildBeforeAfterLogItem(
+                    "title",
+                    "descriptiondescriptiondescc",
+                    "titletitletitle",
+                    "descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondesc",
+                  );
+                },
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildBeforeAfterLogItem(
+    String title,
+    String description,
+    String title2,
+    String description2,
+  ) {
+    return IntrinsicHeight(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  textAlign: TextAlign.start,
+                  style: buildTextStyle(
+                    fontSize: 18,
+                    fontColor: MyColors.lightBlack02,
+                    fontWeight: 700,
+                  ),
+                ),
+                Spacings.vert(6),
+                Text(
+                  description,
+                  maxLines: null,
+                  style: buildTextStyle(
+                    fontSize: 16,
+                    fontColor: MyColors.lightBlack02,
+                    fontWeight: 400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Spacings.horz(12),
+          // const VerticalDivider(
+          //   thickness: 0.4,
+          //   width: 24,
+          //   color: MyColors.yellow01,
+          // ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title2,
+                  textAlign: TextAlign.start,
+                  style: buildTextStyle(
+                    fontSize: 18,
+                    fontColor: MyColors.lightBlack02,
+                    fontWeight: 700,
+                  ),
+                ),
+                Spacings.vert(6),
+                Text(
+                  description2,
+                  maxLines: null,
+                  style: buildTextStyle(
+                    fontSize: 16,
+                    fontColor: MyColors.lightBlack02,
+                    fontWeight: 400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
