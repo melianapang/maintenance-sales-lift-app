44c3ea8be22466555f0defd277d0931d6fee1aa9
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-03-27 01:03:05 +0700

    chore: add viewmodel

--- lib/core/viewmodels/maintenance/detail_history_maintenance_view_model.dart
+++ lib/core/viewmodels/maintenance/detail_history_maintenance_view_model.dart
@@ -1,7 +1,7 @@
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
 
-class DetailUserViewModel extends BaseViewModel {
-  DetailUserViewModel();
+class DetailHistoryMaintenanceViewModel extends BaseViewModel {
+  DetailHistoryMaintenanceViewModel();
 
   @override
   Future<void> initModel() async {}
