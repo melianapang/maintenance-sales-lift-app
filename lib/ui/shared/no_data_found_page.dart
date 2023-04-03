c3e34732255c92ec32484e5ee91b77cae32c5aa4
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-03 21:12:09 +0700

    feat(no-data-page): add lottie to the page

--- lib/ui/shared/no_data_found_page.dart
+++ lib/ui/shared/no_data_found_page.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:rejo_jaya_sakti_apps/core/app_constants/colors.dart';
 import 'package:rejo_jaya_sakti_apps/core/utilities/text_styles.dart';
 
 Widget buildNoDataFoundPage() {
   return Expanded(
-    child: Center(
-      child: Text(
-        "Tidak ada data yang dapat ditampilkan.",
-        textAlign: TextAlign.center,
-        maxLines: 3,
-        style: buildTextStyle(
-          fontSize: 24,
-          fontColor: MyColors.greyButtonBorder,
-          fontWeight: 400,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        LottieBuilder.asset(
+          'assets/lotties/no-data.json',
+          repeat: true,
+          width: 200,
+          height: 200,
+          alignment: Alignment.center,
         ),
-      ),
+        Text(
+          "Tidak ada data yang dapat ditampilkan.",
+          textAlign: TextAlign.center,
+          maxLines: 3,
+          style: buildTextStyle(
+            fontSize: 24,
+            fontColor: MyColors.greyButtonBorder,
+            fontWeight: 400,
+          ),
+        ),
+      ],
     ),
   );
 }
