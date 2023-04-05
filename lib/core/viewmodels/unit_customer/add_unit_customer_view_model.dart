e0d4998e8d150f7d964984d1989f16e4d14b9ca4
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-05 21:59:02 +0700

    feat(add-unit-customer): create bottomsheet for choosing project

--- lib/core/viewmodels/unit_customer/add_unit_customer_view_model.dart
+++ lib/core/viewmodels/unit_customer/add_unit_customer_view_model.dart
@@ -1,9 +1,74 @@
+import 'package:rejo_jaya_sakti_apps/core/apis/api.dart';
+import 'package:rejo_jaya_sakti_apps/core/models/customers/customer_dto.dart';
+import 'package:rejo_jaya_sakti_apps/core/models/pagination_control_model.dart';
+import 'package:rejo_jaya_sakti_apps/core/services/dio_service.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
-import 'package:rejo_jaya_sakti_apps/ui/widgets/filter_menu.dart';
 
 class AddUnitCustomerViewModel extends BaseViewModel {
-  AddUnitCustomerViewModel();
+  AddUnitCustomerViewModel({
+    required DioService dioService,
+  }) : _apiService = ApiService(
+          api: Api(
+            dioService.getDioJwt(),
+          ),
+        );
+
+  final ApiService _apiService;
+
+  List<CustomerData>? _listCustomer;
+  List<CustomerData>? get listCustomer => _listCustomer;
+
+  PaginationControl _paginationControl = PaginationControl();
+  PaginationControl get paginationControl => _paginationControl;
+
+  bool _isShowNoDataFoundPage = false;
+  bool get isShowNoDataFoundPage => _isShowNoDataFoundPage;
+
+  CustomerData? _selectedProyek;
+  CustomerData? get selectedProyek => _selectedProyek;
 
   @override
-  Future<void> initModel() async {}
+  Future<void> initModel() async {
+    setBusy(true);
+
+    paginationControl.currentPage = 1;
+
+    await requestGetAllCustomer();
+    if (_listCustomer?.isEmpty == true || _listCustomer == null) {
+      _isShowNoDataFoundPage = true;
+      notifyListeners();
+    }
+
+    setBusy(false);
+  }
+
+  void setSelectedPIlihProyek({
+    required int selectedMenu,
+  }) {
+    notifyListeners();
+  }
+
+  Future<void> requestGetAllCustomer() async {
+    List<CustomerData>? list = await _apiService.getAllCustomer(
+      _paginationControl.currentPage,
+      _paginationControl.pageSize,
+    );
+
+    if (list != null || list?.isNotEmpty == true) {
+      if (_paginationControl.currentPage == 1) {
+        _listCustomer = list;
+      } else {
+        _listCustomer?.addAll(list!);
+      }
+      _paginationControl.currentPage += 1;
+      notifyListeners();
+    }
+  }
+
+  void setSelectedProyek({
+    required int selectedIndex,
+  }) {
+    _selectedProyek = _listCustomer?[selectedIndex];
+    notifyListeners();
+  }
 }
--- lib/ui/views/unit_customer/add_unit_customer_view.dart
+++ lib/ui/views/unit_customer/add_unit_customer_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:rejo_jaya_sakti_apps/core/app_constants/colors.dart';
+import 'package:rejo_jaya_sakti_apps/core/services/dio_service.dart';
 import 'package:rejo_jaya_sakti_apps/core/utilities/padding_utils.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/unit_customer/add_unit_customer_view_model.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/view_model.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/app_bars.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/spacings.dart';
 import 'package:rejo_jaya_sakti_apps/ui/widgets/buttons.dart';
+import 'package:rejo_jaya_sakti_apps/ui/widgets/cards.dart';
 import 'package:rejo_jaya_sakti_apps/ui/widgets/dialogs.dart';
 import 'package:rejo_jaya_sakti_apps/ui/widgets/text_inputs.dart';
 
@@ -19,7 +25,12 @@ class _AddUnitCustomerViewState extends State<AddUnitCustomerView> {
   @override
   Widget build(BuildContext context) {
     return ViewModel(
-      model: AddUnitCustomerViewModel(),
+      model: AddUnitCustomerViewModel(
+        dioService: Provider.of<DioService>(context),
+      ),
+      onModelReady: (AddUnitCustomerViewModel model) async {
+        await model.initModel();
+      },
       builder: (context, model, _) {
         return Scaffold(
           appBar: buildDefaultAppBar(
@@ -81,12 +92,77 @@ class _AddUnitCustomerViewState extends State<AddUnitCustomerView> {
                     onChangedListener: (text) {},
                   ),
                   Spacings.vert(24),
+                  GestureDetector(
+                    onTap: () {
+                      _showPilihProyekBottomDialog(
+                        context,
+                        model,
+                        setSelectedMenu: model.setSelectedProyek,
+                      );
+                    },
+                    child: TextInput.disabled(
+                      label: "Pilih Proyek",
+                      text: model.selectedProyek?.customerName,
+                      hintText: "Pilih Proyek untuk Unit ini",
+                      suffixIcon: const Icon(
+                        PhosphorIcons.caretDownBold,
+                        color: MyColors.lightBlack02,
+                      ),
+                    ),
+                  ),
+                  Spacings.vert(24),
                 ],
               ),
             ),
           ),
         );
       },
+    );
+  }
+
+  void _showPilihProyekBottomDialog(
+    BuildContext context,
+    AddUnitCustomerViewModel model, {
+    required void Function({
+      required int selectedIndex,
+    })
+        setSelectedMenu,
+  }) {
+    showGeneralBottomSheet(
+      context: context,
+      title: 'Daftar Proyek',
+      isFlexible: false,
+      showCloseButton: false,
+      sizeToScreenRatio: 0.8,
+      child: Expanded(
+        child: LazyLoadScrollView(
+          onEndOfPage: () => model.requestGetAllCustomer(),
+          scrollDirection: Axis.vertical,
+          child: ListView.separated(
+            shrinkWrap: true,
+            itemCount: model.listCustomer?.length ?? 0,
+            separatorBuilder: (_, __) => const Divider(
+              color: MyColors.transparent,
+              height: 20,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              return CustomCardWidget(
+                cardType: CardType.list,
+                title: model.listCustomer?[index].customerName ?? "",
+                description: model.listCustomer?[index].companyName,
+                desc2Size: 16,
+                titleSize: 20,
+                onTap: () {
+                  setSelectedMenu(
+                    selectedIndex: index,
+                  );
+                  Navigator.maybePop(context);
+                },
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
