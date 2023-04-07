3ab61dcd429a2da32ff0e6a2e13a9fceafa83368
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-07 22:58:38 +0700

    feat(export-maintenance): connect it with export data utils and dummy api

--- lib/core/viewmodels/maintenance/export_data_maintenance_view_model.dart
+++ lib/core/viewmodels/maintenance/export_data_maintenance_view_model.dart
@@ -1,12 +1,25 @@
+import 'package:rejo_jaya_sakti_apps/core/apis/api.dart';
+import 'package:rejo_jaya_sakti_apps/core/services/dio_service.dart';
+import 'package:rejo_jaya_sakti_apps/core/utilities/export_data_utils.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
 
 class ExportDataMaintenanceViewModel extends BaseViewModel {
-  ExportDataMaintenanceViewModel();
+  ExportDataMaintenanceViewModel({
+    required DioService dioService,
+  }) : _apiService = ApiService(
+          api: Api(
+            dioService.getDioJwt(),
+          ),
+        );
+
+  final ApiService _apiService;
 
   List<DateTime> _selectedDates = [
     DateTime.now(),
   ];
   List<DateTime> get selectedDates => _selectedDates;
+
+  String? _exportedFileName;
 
   @override
   Future<void> initModel() async {}
@@ -14,5 +27,30 @@ class ExportDataMaintenanceViewModel extends BaseViewModel {
   void setSelectedDates(List<DateTime> value) {
     _selectedDates = value;
     notifyListeners();
+  }
+
+  Future<void> requestExportData() async {
+    setBusy(true);
+    String filePath = await _apiService.requestExportCustomerData();
+    await _exportData(
+      filePath: filePath,
+    );
+    setBusy(false);
+  }
+
+  Future<void> _exportData({
+    required String filePath,
+  }) async {
+    _exportedFileName = await ExportDataUtils.exportData(
+      prefixString: "maintenance_data",
+      filePath: filePath,
+    );
+  }
+
+  Future<void> openExportedData() async {
+    if (_exportedFileName == null) return;
+    await ExportDataUtils.openExportedData(
+      fileName: _exportedFileName ?? "",
+    );
   }
 }
