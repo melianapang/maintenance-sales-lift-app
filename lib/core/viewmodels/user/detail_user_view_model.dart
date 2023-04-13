20d1c33d4916bbf31117e7fd9784e3f74ee260a6
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-13 23:17:03 +0700

    feat(detail-user-vm): connect with api

--- lib/core/viewmodels/user/detail_user_view_model.dart
+++ lib/core/viewmodels/user/detail_user_view_model.dart
@@ -1,5 +1,7 @@
 import 'package:rejo_jaya_sakti_apps/core/apis/api.dart';
+import 'package:rejo_jaya_sakti_apps/core/models/role/role_model.dart';
 import 'package:rejo_jaya_sakti_apps/core/models/user/user_dto.dart';
+import 'package:rejo_jaya_sakti_apps/core/services/authentication_service.dart';
 import 'package:rejo_jaya_sakti_apps/core/services/dio_service.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
 
@@ -7,23 +9,38 @@ class DetailUserViewModel extends BaseViewModel {
   DetailUserViewModel({
     UserData? userData,
     required DioService dioService,
+    required AuthenticationService authenticationService,
   })  : _userData = userData,
         _apiService = ApiService(
           api: Api(
             dioService.getDioJwt(),
           ),
-        );
+        ),
+        _authenticationService = authenticationService;
 
   final ApiService _apiService;
+  final AuthenticationService _authenticationService;
 
   UserData? _userData;
   UserData? get userData => _userData;
+
+  bool _isAllowedToDeleteUser = false;
+  bool get isAllowedToDeleteUser => _isAllowedToDeleteUser;
 
   String? _errorMsg = "";
   String? get errorMsg => _errorMsg;
 
   @override
-  Future<void> initModel() async {}
+  Future<void> initModel() async {
+    setBusy(true);
+    await _checkIsAllowedToDeleteUser();
+    setBusy(false);
+  }
+
+  Future<void> _checkIsAllowedToDeleteUser() async {
+    Role role = await _authenticationService.getUserRole();
+    _isAllowedToDeleteUser = role == Role.Admin || role == Role.SuperAdmin;
+  }
 
   Future<bool> requestDeleteUser() async {
     final response = await _apiService.requestDeleteUser(
