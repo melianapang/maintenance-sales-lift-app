069b075875d79533ecef06d9a5e90c60b61399e9
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-13 20:28:31 +0700

    feat(location-utils): implement geolocator

---
+++ lib/core/utilities/location_utils.dart
@@ -0,0 +1,60 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationUtils {
+  static final GeolocatorPlatform _geolocatorPlatform =
+      GeolocatorPlatform.instance;
+
+  static Future<Position?> getCurrentPosition() async {
+    final hasPermission = await _handlePermission();
+
+    if (!hasPermission) {
+      return null;
+    }
+
+    return await _geolocatorPlatform.getCurrentPosition();
+  }
+
+  static Future<bool> _handlePermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await _geolocatorPlatform.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return false;
+    }
+
+    permission = await _geolocatorPlatform.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await _geolocatorPlatform.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return false;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return false;
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return true;
+  }
+
+  Future<Position?> _getLastKnownPosition() async {
+    final position = await _geolocatorPlatform.getLastKnownPosition();
+    if (position != null) {
+      return position;
+    }
+    return null;
+  }
+}
