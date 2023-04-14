6c5737fdebc29c15c8c6bbbfd4ee61ee525946d9
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-15 00:15:36 +0700

    feat(unit-dto): update dto

--- lib/core/models/unit_customer/unit_customer_model.dart
+++ lib/core/models/unit_customer/unit_customer_model.dart
@@ -1,11 +1,1 @@
-class UnitData {
-  UnitData({
-    required this.unitId,
-    required this.unitName,
-    required this.location,
-  });
 
-  String unitId;
-  String unitName;
-  String location;
-}
---
+++ lib/core/models/unit_customer/unit_dto.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'unit_dto.g.dart';
+
+//region get all unit
+@JsonSerializable()
+class GetAllUnitResponse {
+  GetAllUnitResponse({
+    required this.isSuccess,
+    required this.message,
+    required this.data,
+  });
+
+  factory GetAllUnitResponse.fromJson(Map<String, dynamic> json) =>
+      _$GetAllUnitResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GetAllUnitResponseToJson(this);
+
+  @JsonKey(name: "Success")
+  final bool isSuccess;
+
+  @JsonKey(name: "Message")
+  final String message;
+
+  @JsonKey(name: "Data")
+  final ListUnitData data;
+}
+
+@JsonSerializable()
+class ListUnitData {
+  ListUnitData({
+    required this.totalSize,
+    required this.result,
+  });
+
+  factory ListUnitData.fromJson(Map<String, dynamic> json) =>
+      _$ListUnitDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ListUnitDataToJson(this);
+
+  @JsonKey(name: "total_size")
+  final String totalSize;
+
+  @JsonKey(name: "result")
+  final List<UnitData> result;
+}
+
+@JsonSerializable()
+class UnitData {
+  UnitData({
+    required this.unitId,
+    required this.unitName,
+    required this.unitLocation,
+  });
+
+  factory UnitData.fromJson(Map<String, dynamic> json) =>
+      _$UnitDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UnitDataToJson(this);
+
+  @JsonKey(name: "unit_id")
+  final String unitId;
+
+  @JsonKey(name: "unit_name")
+  final String unitName;
+
+  @JsonKey(name: "unit_location")
+  final String unitLocation;
+}
+//endregion
