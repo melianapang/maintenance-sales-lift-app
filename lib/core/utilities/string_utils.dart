2bb65fa589ff9634adf1a462a592a183d783868c
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-14 22:05:27 +0700

    fix(string-utils): fix replaceUnderscoreToSpaceAndTitleCase only for non-numeric and handle if the word only contain one alphabet

--- lib/core/utilities/string_utils.dart
+++ lib/core/utilities/string_utils.dart
@@ -5,8 +5,11 @@ class StringUtils {
   static String replaceUnderscoreToSpaceAndTitleCase(String value) {
     final String withoutUnderscores = value.replaceAll('_', ' ');
     final List<String> words = withoutUnderscores.split(' ');
-    for (int i = 0; i < words.length; i++) {
-      words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+    if (!isNumericString(words.first) && withoutUnderscores.isNotEmpty) {
+      for (int i = 0; i < words.length; i++) {
+        if (words[i].length <= 1) break;
+        words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+      }
     }
     return words.join(' ');
   }
@@ -17,5 +20,9 @@ class StringUtils {
 
   static String capitalize(String data) {
     return "${data[0].toUpperCase()}${data.substring(1).toLowerCase()}";
+  }
+
+  static bool isNumericString(String value) {
+    return double.tryParse(value) != null;
   }
 }
