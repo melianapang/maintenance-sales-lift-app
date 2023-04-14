0e0f4d32285581b42ebe96c27fe016dec3cf04f1
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-14 22:28:46 +0700

    feat(list-log-vm): connect with api

--- lib/core/viewmodels/log/list_log_view_model.dart
+++ lib/core/viewmodels/log/list_log_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:rejo_jaya_sakti_apps/core/apis/api.dart';
 import 'package:rejo_jaya_sakti_apps/core/models/log/log_dto.dart';
+import 'package:rejo_jaya_sakti_apps/core/models/pagination_control_model.dart';
 import 'package:rejo_jaya_sakti_apps/core/services/dio_service.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
 
@@ -17,20 +18,49 @@ class ListLogViewModel extends BaseViewModel {
   List<LogData>? _listLogData;
   List<LogData>? get listLogData => _listLogData;
 
+  PaginationControl _paginationControl = PaginationControl();
+  PaginationControl get paginationControl => _paginationControl;
+
   bool _isShowNoDataFoundPage = false;
   bool get isShowNoDataFoundPage => _isShowNoDataFoundPage;
+
+  String? _errorMsg = "";
+  String? get errorMsg => _errorMsg;
 
   @override
   Future<void> initModel() async {
     setBusy(true);
-    _listLogData = await _apiService.requestGetAllLog();
+
+    paginationControl.currentPage = 1;
+
+    await requestGetAllLog();
+
     if (_listLogData?.isEmpty == true || _listLogData == null) {
       _isShowNoDataFoundPage = true;
       notifyListeners();
     }
-
-    print("logDataa keys: ${_listLogData?[0].contentsOld?.values}");
-    print("logDataa entries: ${_listLogData?[0].contentsNew?.values}");
     setBusy(false);
+  }
+
+  Future<void> requestGetAllLog() async {
+    final response = await _apiService.requestGetAllLog(
+      currentPage: _paginationControl.currentPage,
+      pageSize: _paginationControl.pageSize,
+    );
+
+    if (response.isRight) {
+      if (response.right != null || response.right?.isNotEmpty == true) {
+        if (_paginationControl.currentPage == 1) {
+          _listLogData = response.right!;
+        } else {
+          _listLogData?.addAll(response.right!);
+        }
+        _paginationControl.currentPage += 1;
+        notifyListeners();
+      }
+      return;
+    }
+
+    _errorMsg = response.left.message;
   }
 }
