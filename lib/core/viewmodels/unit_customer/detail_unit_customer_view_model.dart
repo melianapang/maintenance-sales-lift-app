3046551d6f61ef7a3251b920e0ba1473e66c20ca
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-15 00:17:57 +0700

    feat(detail-unit): remove unused imports

--- lib/core/viewmodels/unit_customer/detail_unit_customer_view_model.dart
+++ lib/core/viewmodels/unit_customer/detail_unit_customer_view_model.dart
@@ -1,6 +1,5 @@
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:rejo_jaya_sakti_apps/core/apis/api.dart';
-import 'package:rejo_jaya_sakti_apps/core/models/unit_customer/unit_customer_model.dart';
+import 'package:rejo_jaya_sakti_apps/core/models/unit_customer/unit_dto.dart';
 import 'package:rejo_jaya_sakti_apps/core/services/dio_service.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
 
