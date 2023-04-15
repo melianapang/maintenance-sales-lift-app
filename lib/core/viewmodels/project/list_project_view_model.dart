2728c86b1f25abcd9175318a37e00b1405278ca1
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-15 13:40:23 +0700

    feat(list-project): connect with api

--- lib/core/viewmodels/project/list_project_view_model.dart
+++ lib/core/viewmodels/project/list_project_view_model.dart
@@ -1,21 +1,66 @@
+import 'package:rejo_jaya_sakti_apps/core/apis/api.dart';
+import 'package:rejo_jaya_sakti_apps/core/models/pagination_control_model.dart';
+import 'package:rejo_jaya_sakti_apps/core/models/project/project_dto.dart';
+import 'package:rejo_jaya_sakti_apps/core/services/dio_service.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
 
 class ListProjectViewModel extends BaseViewModel {
-  ListProjectViewModel();
+  ListProjectViewModel({
+    required DioService dioService,
+  }) : _apiService = ApiService(
+          api: Api(
+            dioService.getDioJwt(),
+          ),
+        );
 
-  List<String> _listProject = [];
-  List<String> get listProject => _listProject;
+  final ApiService _apiService;
+
+  PaginationControl _paginationControl = PaginationControl();
+  PaginationControl get paginationControl => _paginationControl;
 
   bool _isShowNoDataFoundPage = false;
   bool get isShowNoDataFoundPage => _isShowNoDataFoundPage;
 
+  String? _errorMsg = "";
+  String? get errorMsg => _errorMsg;
+
+  List<ProjectData> _listProject = [];
+  List<ProjectData> get listProject => _listProject;
+
   @override
   Future<void> initModel() async {
     setBusy(true);
-    if (_listProject.isEmpty) {
+
+    paginationControl.currentPage = 1;
+
+    await requestGetAllProjects();
+
+    if (_listProject.isEmpty == true) {
       _isShowNoDataFoundPage = true;
       notifyListeners();
     }
     setBusy(false);
+  }
+
+  Future<void> requestGetAllProjects() async {
+    final response = await _apiService.getAllProjects(
+      currentPage: _paginationControl.currentPage,
+      pageSize: _paginationControl.pageSize,
+    );
+
+    if (response.isRight) {
+      if (response.right != null || response.right?.isNotEmpty == true) {
+        if (_paginationControl.currentPage == 1) {
+          _listProject = response.right!;
+        } else {
+          _listProject.addAll(response.right!);
+        }
+        _paginationControl.currentPage += 1;
+        notifyListeners();
+      }
+      return;
+    }
+
+    _errorMsg = response.left.message;
   }
 }
--- lib/ui/views/project/list_project_view.dart
+++ lib/ui/views/project/list_project_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
+import 'package:provider/provider.dart';
 import 'package:rejo_jaya_sakti_apps/core/app_constants/colors.dart';
 import 'package:rejo_jaya_sakti_apps/core/app_constants/routes.dart';
+import 'package:rejo_jaya_sakti_apps/core/services/dio_service.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/project/list_project_view_model.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/view_model.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/app_bars.dart';
@@ -12,7 +15,9 @@ import 'package:rejo_jaya_sakti_apps/ui/shared/spacings.dart';
 import 'package:rejo_jaya_sakti_apps/ui/widgets/cards.dart';
 
 class ListProjectView extends StatefulWidget {
-  const ListProjectView({super.key});
+  const ListProjectView({
+    super.key,
+  });
 
   @override
   State<ListProjectView> createState() => _ListProjectViewState();
@@ -22,7 +27,9 @@ class _ListProjectViewState extends State<ListProjectView> {
   @override
   Widget build(BuildContext context) {
     return ViewModel(
-      model: ListProjectViewModel(),
+      model: ListProjectViewModel(
+        dioService: Provider.of<DioService>(context),
+      ),
       onModelReady: (ListProjectViewModel model) async {
         await model.initModel();
       },
@@ -50,25 +57,29 @@ class _ListProjectViewState extends State<ListProjectView> {
               Spacings.vert(12),
               if (!model.isShowNoDataFoundPage && !model.busy)
                 Expanded(
-                  child: ListView.separated(
-                    shrinkWrap: true,
-                    itemCount: 10,
-                    separatorBuilder: (_, __) => const Divider(
-                      color: MyColors.transparent,
-                      height: 20,
+                  child: LazyLoadScrollView(
+                    onEndOfPage: () => model.requestGetAllProjects(),
+                    scrollDirection: Axis.vertical,
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      itemCount: model.listProject.length,
+                      separatorBuilder: (_, __) => const Divider(
+                        color: MyColors.transparent,
+                        height: 20,
+                      ),
+                      itemBuilder: (BuildContext context, int index) {
+                        return CustomCardWidget(
+                          cardType: CardType.list,
+                          title: model.listProject[index].projectName,
+                          description: model.listProject[index].customerName,
+                          titleSize: 20,
+                          descSize: 16,
+                          onTap: () {
+                            Navigator.pushNamed(context, Routes.detailProject);
+                          },
+                        );
+                      },
                     ),
-                    itemBuilder: (BuildContext context, int index) {
-                      return CustomCardWidget(
-                        cardType: CardType.list,
-                        title: "KA-23243",
-                        description: "PT. ABC JAYA",
-                        titleSize: 20,
-                        descSize: 16,
-                        onTap: () {
-                          Navigator.pushNamed(context, Routes.detailProject);
-                        },
-                      );
-                    },
                   ),
                 ),
               if (model.isShowNoDataFoundPage && !model.busy)
