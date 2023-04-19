17f8a84c0c9d084d755bde8eff065a8e9c2ccf44
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-20 00:37:59 +0700

    feat(detail-history-follow-up): add dummy photo

--- lib/ui/views/follow_up/detail_history_follow_up_view.dart
+++ lib/ui/views/follow_up/detail_history_follow_up_view.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:rejo_jaya_sakti_apps/core/app_constants/colors.dart';
+import 'package:rejo_jaya_sakti_apps/core/models/follow%20up/follow_up_dto.dart';
+import 'package:rejo_jaya_sakti_apps/core/models/gallery_data_model.dart';
+import 'package:rejo_jaya_sakti_apps/core/utilities/date_time_utils.dart';
 import 'package:rejo_jaya_sakti_apps/core/utilities/text_styles.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/follow_up/detail_history_follow_up_view_model.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/view_model.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/app_bars.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/spacings.dart';
+import 'package:rejo_jaya_sakti_apps/ui/widgets/gallery.dart';
 import 'package:rejo_jaya_sakti_apps/ui/widgets/status_card.dart';
 import 'package:rejo_jaya_sakti_apps/ui/widgets/text_inputs.dart';
+import 'package:intl/intl.dart';
+
+class DetailHistoryFollowUpViewParam {
+  DetailHistoryFollowUpViewParam({
+    this.historyData,
+  });
+
+  final HistoryFollowUpData? historyData;
+}
 
 class DetailHistoryFollowUpView extends StatefulWidget {
-  const DetailHistoryFollowUpView({super.key});
+  const DetailHistoryFollowUpView({
+    required this.param,
+    super.key,
+  });
+
+  final DetailHistoryFollowUpViewParam param;
 
   @override
   State<DetailHistoryFollowUpView> createState() =>
@@ -17,10 +35,25 @@ class DetailHistoryFollowUpView extends StatefulWidget {
 }
 
 class _DetailHistoryFollowUpViewState extends State<DetailHistoryFollowUpView> {
+  List<GalleryData> galleryData = [
+    GalleryData(
+      filepath:
+          "https://media1.popsugar-assets.com/files/thumbor/0ebv7kCHr0T-_O3RfQuBoYmUg1k/475x60:1974x1559/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2019/09/09/023/n/1922398/9f849ffa5d76e13d154137.01128738_/i/Taylor-Swift.jpg",
+      galleryType: GalleryType.PHOTO,
+    ),
+    GalleryData(
+      filepath:
+          "https://media.glamour.com/photos/618e9260d0013b8dece7e9d8/master/w_2560%2Cc_limit/GettyImages-1236509084.jpg",
+      galleryType: GalleryType.PHOTO,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ViewModel(
-        model: DetailHistoryFollowUpViewModel(),
+        model: DetailHistoryFollowUpViewModel(
+          historyData: widget.param.historyData,
+        ),
         onModelReady: (DetailHistoryFollowUpViewModel model) async {
           await model.initModel();
         },
@@ -43,7 +76,7 @@ class _DetailHistoryFollowUpViewState extends State<DetailHistoryFollowUpView> {
                   children: [
                     Spacings.vert(20),
                     Text(
-                      "Nadia Ang",
+                      model.historyData?.customerName ?? "",
                       style: buildTextStyle(
                         fontSize: 32,
                         fontWeight: 800,
@@ -51,7 +84,7 @@ class _DetailHistoryFollowUpViewState extends State<DetailHistoryFollowUpView> {
                       ),
                     ),
                     Text(
-                      "PT ABC JAYA",
+                      model.historyData?.companyName ?? "",
                       style: buildTextStyle(
                         fontSize: 20,
                         fontWeight: 400,
@@ -65,15 +98,42 @@ class _DetailHistoryFollowUpViewState extends State<DetailHistoryFollowUpView> {
                     ),
                     Spacings.vert(35),
                     TextInput.disabled(
-                      label: "Tanggal",
+                      label: "Tanggal Konfirmasi",
+                      text: DateTimeUtils.convertStringToOtherStringDateFormat(
+                        date: model.historyData?.scheduleDate ??
+                            DateTimeUtils.convertDateToString(
+                              date: DateTime.now(),
+                              formatter: DateFormat(
+                                DateTimeUtils.DATE_FORMAT_2,
+                              ),
+                            ),
+                        formattedString: DateTimeUtils.DATE_FORMAT_2,
+                      ),
                     ),
                     Spacings.vert(24),
-                    TextInput.disabledMultiline(
+                    TextInput.disabled(
                       label: "Catatan",
-                      text:
-                          "CatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatanCatatan",
+                      hintText: "Catatan Riwayat Konfirmasi",
+                      text: model.historyData?.note ?? "",
                     ),
                     Spacings.vert(24),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Foto",
+                        style: buildTextStyle(
+                          fontSize: 14,
+                          fontWeight: 400,
+                          fontColor: MyColors.lightBlack02,
+                        ),
+                      ),
+                    ),
+                    Spacings.vert(8),
+                    GalleryThumbnailWidget(
+                      isCRUD: false,
+                      galleryData: galleryData,
+                      galleryType: GalleryType.PHOTO,
+                    ),
                   ],
                 ),
               ),
