2a10eeb9a53091b1c4757b98495e13fdf556fa9f
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-20 00:39:49 +0700

    feat(form-follow-up): update follow up result options

--- lib/core/viewmodels/follow_up/form_follow_up_view_model.dart
+++ lib/core/viewmodels/follow_up/form_follow_up_view_model.dart
@@ -1,20 +1,23 @@
+import 'package:rejo_jaya_sakti_apps/core/models/customers/customer_dto.dart';
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/base_view_model.dart';
 import 'package:rejo_jaya_sakti_apps/ui/widgets/filter_menu.dart';
 
 class FormFollowUpViewModel extends BaseViewModel {
-  FormFollowUpViewModel();
+  FormFollowUpViewModel({
+    CustomerData? customerData,
+  }) : _customerData = customerData;
+
+  CustomerData? _customerData;
+  CustomerData? get customerData => _customerData;
 
   // Filter related
   int _selectedHasilKonfirmasiOption = 0;
   int get selectedHasilKonfirmasiOption => _selectedHasilKonfirmasiOption;
   final List<FilterOption> _hasilKonfirmasiOption = [
-    FilterOption("Konfirmasi Pertama", true),
-    FilterOption("Konfirmasi Kedua", false),
-    FilterOption("Konfirmasi Ketiga", false),
-    FilterOption("Negosiasi", false),
-    FilterOption("Instalasi", false),
-    FilterOption("Selesai", false),
-    FilterOption("Batal", false),
+    FilterOption("Loss", true),
+    FilterOption("Win", false),
+    FilterOption("Hot", false),
+    FilterOption("In Progress", false),
   ];
   List<FilterOption> get hasilKonfirmasiOption => _hasilKonfirmasiOption;
   // End of filter related
@@ -25,7 +28,10 @@ class FormFollowUpViewModel extends BaseViewModel {
   List<DateTime> get selectedDates => _selectedDates;
 
   @override
-  Future<void> initModel() async {}
+  Future<void> initModel() async {
+    setBusy(true);
+    setBusy(false);
+  }
 
   void setHasilKonfirmasi(int index) {
     _selectedHasilKonfirmasiOption = index;
