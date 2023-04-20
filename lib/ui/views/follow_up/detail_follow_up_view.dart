054b9bf4dac5094623b0f344a784d3f4621c0e11
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-04-20 21:47:40 +0700

    fix(detail-foll-up): add loading page

--- lib/ui/views/follow_up/detail_follow_up_view.dart
+++ lib/ui/views/follow_up/detail_follow_up_view.dart
@@ -9,6 +9,7 @@ import 'package:rejo_jaya_sakti_apps/core/viewmodels/follow_up/detail_follow_up_
 import 'package:rejo_jaya_sakti_apps/core/viewmodels/view_model.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/app_bars.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/floating_button.dart';
+import 'package:rejo_jaya_sakti_apps/ui/shared/loading.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/spacings.dart';
 import 'package:rejo_jaya_sakti_apps/ui/views/follow_up/form_follow_up_view.dart';
 import 'package:rejo_jaya_sakti_apps/ui/widgets/timeline.dart';
@@ -107,63 +108,65 @@ class _DetailFollowUpViewState extends State<DetailFollowUpView> {
           ),
           body: SingleChildScrollView(
             padding: const EdgeInsets.all(24.0),
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    model.customerName ?? "",
-                    style: buildTextStyle(
-                      fontSize: 26,
-                      fontWeight: 800,
-                      fontColor: MyColors.yellow01,
-                    ),
-                  ),
-                ),
-                Spacings.vert(10),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    model.companyName ?? "",
-                    style: buildTextStyle(
-                      fontSize: 20,
-                      fontWeight: 400,
-                      fontColor: MyColors.lightBlack02,
-                    ),
-                  ),
-                ),
-                Spacings.vert(38),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Konfirmasi Terakhir",
-                    style: buildTextStyle(
-                      fontSize: 16,
-                      fontColor: MyColors.yellow01,
-                      fontWeight: 400,
-                    ),
-                  ),
-                ),
-                if (model.timelineData.isNotEmpty)
-                  TimelineWidget(
-                    listTimeline: model.timelineData,
-                  ),
-                if (model.timelineData.isEmpty)
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Belum ada Riwayat Konfirmasi untuk unit ini.",
-                      style: buildTextStyle(
-                        fontSize: 16,
-                        fontColor: MyColors.lightBlack02.withOpacity(
-                          0.5,
+            child: !model.busy
+                ? Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          model.customerName ?? "",
+                          style: buildTextStyle(
+                            fontSize: 26,
+                            fontWeight: 800,
+                            fontColor: MyColors.yellow01,
+                          ),
                         ),
-                        fontWeight: 300,
                       ),
-                    ),
-                  ),
-              ],
-            ),
+                      Spacings.vert(10),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          model.companyName ?? "",
+                          style: buildTextStyle(
+                            fontSize: 20,
+                            fontWeight: 400,
+                            fontColor: MyColors.lightBlack02,
+                          ),
+                        ),
+                      ),
+                      Spacings.vert(38),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Konfirmasi Terakhir",
+                          style: buildTextStyle(
+                            fontSize: 16,
+                            fontColor: MyColors.yellow01,
+                            fontWeight: 400,
+                          ),
+                        ),
+                      ),
+                      if (model.timelineData.isNotEmpty)
+                        TimelineWidget(
+                          listTimeline: model.timelineData,
+                        ),
+                      if (model.timelineData.isEmpty)
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Belum ada Riwayat Konfirmasi untuk unit ini.",
+                            style: buildTextStyle(
+                              fontSize: 16,
+                              fontColor: MyColors.lightBlack02.withOpacity(
+                                0.5,
+                              ),
+                              fontWeight: 300,
+                            ),
+                          ),
+                        ),
+                    ],
+                  )
+                : buildLoadingPage(),
           ),
         );
       },
