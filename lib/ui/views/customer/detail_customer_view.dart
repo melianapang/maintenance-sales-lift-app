89efd7276240a85162649f70a42b60fe06761af0
Meliana Pang <meliana.pangkasidhi@geniebook.com>
2023-06-20 22:54:18 +0700

    refactor(det-cust): remove unused imports

--- lib/ui/views/customer/detail_customer_view.dart
+++ lib/ui/views/customer/detail_customer_view.dart
@@ -15,7 +15,6 @@ import 'package:rejo_jaya_sakti_apps/ui/shared/floating_button.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/loading.dart';
 import 'package:rejo_jaya_sakti_apps/ui/shared/spacings.dart';
 import 'package:rejo_jaya_sakti_apps/ui/views/customer/edit_customer_view.dart';
-import 'package:rejo_jaya_sakti_apps/ui/views/customer/upload_document_view.dart';
 import 'package:rejo_jaya_sakti_apps/ui/views/follow_up/detail_follow_up_view.dart';
 import 'package:rejo_jaya_sakti_apps/ui/views/unit_customer/add_unit_customer_view.dart';
 import 'package:rejo_jaya_sakti_apps/ui/views/unit_customer/list_unit_customer_view.dart';
